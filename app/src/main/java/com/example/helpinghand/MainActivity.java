d27e6044d1eb75a82389f85c68e6bfb7ce71e20f
Ernest <s10204577@connect.np.edu.sg>
2021-11-17 13:43:55 +0800

    created main activity

--- app/src/main/java/com/example/helpinghand/MainActivity.java
+++ app/src/main/java/com/example/helpinghand/MainActivity.java
@@ -6,13 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+abstract class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sosButton = findViewById(R.id.sosButton);
+        sosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.sosButton:
+                        Toast.makeText(getApplicationContext(), "SOS Button activated", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 
-}
+
+    }
+
+
