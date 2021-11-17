9d3e567adb04908115228b4f0fc7e9c0910c1863
Chen-Han-NP <s10202961@connect.np.edu.sg>
2021-11-17 13:53:13 +0800

    sos button

--- app/src/main/java/com/example/helpinghand/MainActivity.java
+++ app/src/main/java/com/example/helpinghand/MainActivity.java
@@ -8,15 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-abstract class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button sosButton = findViewById(R.id.sosButton);
         sosButton.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View view) {
                 switch (view.getId()){
                     case R.id.sosButton:
@@ -25,6 +25,7 @@ abstract class MainActivity extends AppCompatActivity implements View.OnClickLis
                 }
             }
         });
+
     }
 
 
