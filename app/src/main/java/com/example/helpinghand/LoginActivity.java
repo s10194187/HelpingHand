6f37d899b6096f172ed1011508388cbdd177463d
s10194187 <74453144+s10194187@users.noreply.github.com>
2021-11-17 10:08:15 +0800

    test push, create LoginActivity

---
+++ app/src/main/java/com/example/helpinghand/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.helpinghand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        buttonLeft = (Button) findViewById(R.id.buttonLeft);
+
+
+
+
+
+
+    }
+}
