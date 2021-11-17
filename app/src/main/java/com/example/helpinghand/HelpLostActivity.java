7bed22be0e507904bc605d27d718d259c8c26a15
nhaaki <S10206177@connect.np.edu.sg>
2021-11-17 10:56:41 +0800

    Add view for Help Lost activity under disadvantaged section

--- app/src/main/java/com/example/helpinghand/HelpLostActivity.java
+++ app/src/main/java/com/example/helpinghand/HelpLostActivity.java
@@ -3,22 +3,12 @@ package com.example.helpinghand;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class HelpLostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
-        buttonLeft = (Button) findViewById(R.id.buttonLeft);
-
-
-
-
-
-
+        setContentView(R.layout.activity_help_lost);
     }
 }
