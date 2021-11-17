8a4fa288173ac419a58e9c044b1c027dae16307a
nhaaki <S10206177@connect.np.edu.sg>
2021-11-17 11:15:43 +0800

    Add onClick function for back button in HelpLostActivity

--- app/src/main/java/com/example/helpinghand/HelpLostActivity.java
+++ app/src/main/java/com/example/helpinghand/HelpLostActivity.java
@@ -2,7 +2,10 @@ package com.example.helpinghand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpLostActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class HelpLostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_lost);
     }
+
+    Button returnBack = findViewById(R.id.backButton);
+        returnBack.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v)
+        {
+            Intent navigateToPreviousPage = new Intent(HelpLostActivity.this, MainActivity.class);
+            startActivity(navigateToPreviousPage);
+        }
+    });
 }
