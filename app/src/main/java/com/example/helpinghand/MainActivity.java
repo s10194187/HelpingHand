71f4f17a1c975161e28c82a7f3c8614764b47b4a
Ernest <s10204577@connect.np.edu.sg>
2021-11-17 16:03:14 +0800

    Added countdown timer to sos button

--- app/src/main/java/com/example/helpinghand/MainActivity.java
+++ app/src/main/java/com/example/helpinghand/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,17 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button sosButton = findViewById(R.id.sosButton);
-        sosButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                switch (view.getId()){
-                    case R.id.sosButton:
-                        Toast.makeText(getApplicationContext(), "SOS Button activated", Toast.LENGTH_SHORT).show();
-
-                }
-            }
-        });
 
         Button lostButton = findViewById(R.id.lostButton);
         lostButton.setOnClickListener(new View.OnClickListener(){
@@ -44,6 +35,36 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        Button sosButton = findViewById(R.id.sosButton);
+        TextView sosText = findViewById(R.id.sosText);
+        sosButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                switch (view.getId()){
+                    case R.id.sosButton:
+                        Toast.makeText(getApplicationContext(), "SOS Button activated", Toast.LENGTH_LONG).show();
+
+                }
+                new CountDownTimer(9000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        sosText.setText("   " + millisUntilFinished / 1000);
+                    }
+
+                    public void onClick(View view){
+                        cancel();
+                    }
+
+                    public void onFinish() {
+                        sosText.setText("SOS");
+                    }
+                }.start();
+
+
+
+
+
+
         //Button locationButton = findViewById(R.id.locationButton);
         //.setOnClickListener(new View.OnClickListener(){
         //    public void onClick(View v){
@@ -56,6 +77,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    }
+    });}}
 
 
